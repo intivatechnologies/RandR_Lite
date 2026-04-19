2b721d09ac9bb693bbdfa5be496fdb5ebb4bd520
mikesinclair66 <michaelkevin.sinclair@gmail.com>
2026-04-19 14:39:40 -0700

    Reconfigured GH pages for vue3

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/RandR_Lite/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
